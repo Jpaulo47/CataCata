d63804f6c0040073db04583eac6eb358e815565b
João Paulo da Silva Rodrigues <joaopaulo.rodrigues@a4pm.com.br>
2023-03-31 16:16:26 -0300

    Convertendo código java para kotlin

---
+++ app/src/main/java/com/example/catacata/activity/activity/MeusMateriaisActivity.kt
@@ -0,0 +1,52 @@
+package com.example.catacata.activity.activity
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.bumptech.glide.Glide
+import com.example.catacata.R
+import com.example.catacata.activity.helper.UsuarioFirebase
+import com.example.catacata.databinding.ActivityMeusMateriaisBinding
+import com.google.firebase.auth.FirebaseUser
+import de.hdodenhof.circleimageview.CircleImageView
+
+class MeusMateriaisActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMeusMateriaisBinding
+    private lateinit var circleImageToolbarAlternativa: CircleImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMeusMateriaisBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbarAlternativa)
+        toolbar.title = "Meus Materiais"
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_voltar)
+        toolbar.setTitleTextColor(resources.getColor(R.color.verdeIcons))
+
+        circleImageToolbarAlternativa = findViewById(R.id.circleImageToolbar)
+
+        circleImageToolbarAlternativa.setOnClickListener {
+            val intent = Intent(this, PerfilActivity::class.java)
+            startActivity(intent)
+        }
+
+        val usuarioPerfil: FirebaseUser? = UsuarioFirebase.getUsuarioAtual()
+        val url: Uri? = usuarioPerfil?.photoUrl
+        if (url != null) {
+            Glide.with(this).load(url).into(circleImageToolbarAlternativa)
+        } else {
+            circleImageToolbarAlternativa.setImageResource(R.drawable.padrao)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean { // método para finalizar activity atual
+        finish()
+        return false
+    }
+}
