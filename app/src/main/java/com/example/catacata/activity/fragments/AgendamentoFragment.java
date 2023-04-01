9909e8cde3b2d5d37d2f0e25c9977a6c27439faf
João Paulo da Silva Rodrigues <joaopaulo.rodrigues@a4pm.com.br>
2023-03-31 22:23:25 -0300

    initial commit tested

--- app/src/main/java/com/example/catacata/activity/fragments/AgendamentoFragment.java
+++ app/src/main/java/com/example/catacata/activity/fragments/AgendamentoFragment.java
@@ -17,7 +17,7 @@ import com.example.catacata.R;
  */
 public class AgendamentoFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
+    // TODO: Rename parameter hhjharguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
